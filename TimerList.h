7934b7b72685611a7f021628f2c079b166f843cf
Jan C. Bierowiec <93941190+jbierowiec@users.noreply.github.com>
2023-08-14 10:32:47 -0400

    Update TimerList.h

--- TimerList.h
+++ TimerList.h
@@ -9,27 +9,27 @@ using namespace std;
 
 #pragma once
 
-/*** TempList class using templates ***/
+/*** TimerList class using templates ***/
 template<class T>
-class TempList {
+class TimerList {
 public:
    // Constructor with an integer capacity as a parameter
-   TempList(int capacity);
+   TimerList(int capacity);
    // Copy Constructor
-   TempList(const TempList& tl);
+   TimerList(const TimerList& tl);
    // Assignment operator
-   TempList& operator =(const TempList& tl);
+   TimerList& operator =(const TimerList& tl);
    // Destructor
-   ~TempList();
+   ~TimerList();
 
-   // append function to add Timer object to datalist array in TempList
+   // append function to add Timer object to datalist array in TimerList
    void append(const Timer& data);
    // access t_size
    T size() const;
    // check if datalist array is full
    bool isFull() const;
    // friend function overloading << operator
-   template<class U> friend ostream& operator<< (ostream& out, const TempList<U>& list);
+   template<class U> friend ostream& operator<< (ostream& out, const TimerList<U>& list);
 
    // at function returns an element in datalist array
    Timer& at(int index);
@@ -50,21 +50,21 @@ private:
    Dyanmically allocated the datalist array of size t_capacity
 */
 template<class T>
-TempList<T>::TempList(int size) : t_capacity(size), t_size(0)
+TimerList<T>::TimerList(int size) : t_capacity(size), t_size(0)
 {
    datalist = new Timer[t_capacity];
 }
 
 /* Destructor: free the memory of datalist */
 template<class T>
-TempList<T>::~TempList()
+TimerList<T>::~TimerList()
 {
    delete [] datalist;
 }
 
 /* Copy Constructor */
 template<class T>
-TempList<T>::TempList(const TempList<T>& tl) : t_capacity(tl.t_capacity), t_size(tl.t_size)
+TimerList<T>::TimerList(const TimerList<T>& tl) : t_capacity(tl.t_capacity), t_size(tl.t_size)
 {
    datalist = new Timer[t_capacity];
    for (int i = 0; i < t_size; i++) {
@@ -74,7 +74,7 @@ TempList<T>::TempList(const TempList<T>& tl) : t_capacity(tl.t_capacity), t_size
 
 /* Assignment operator */
 template<class T>
-TempList<T>& TempList<T>::operator =(const TempList<T>& tl) 
+TimerList<T>& TimerList<T>::operator =(const TimerList<T>& tl) 
 {
    if (this != &tl)
    {
@@ -93,15 +93,15 @@ TempList<T>& TempList<T>::operator =(const TempList<T>& tl)
 
 /* Overloading ostream class */
 template<class T>
-ostream& operator<<(ostream& out, const TempList<T>& tempList) {
-   for (int i = 0; i < tempList.size(); i++) {
-      out << tempList[i] << endl;
+ostream& operator<<(ostream& out, const TimerList<T>& timerList) {
+   for (int i = 0; i < timerList.size(); i++) {
+      out << timerList[i] << endl;
    }
    return out;
 }
 
 template<class T>
-void TempList<T>::append(const Timer& data)
+void TimerList<T>::append(const Timer& data)
 {
    if (!isFull()) {
       datalist[t_size] = data;
@@ -110,25 +110,23 @@ void TempList<T>::append(const Timer& data)
 }
 
 template<class T>
-Timer& TempList<T>::at(int index)
+Timer& TimerList<T>::at(int index)
 {
    assert(index >= 0 && index < t_size);
    return datalist[index];
 }
 
 template<class T>
-T TempList<T>::size() const {
+T TimerList<T>::size() const {
    return t_size;
 }
 
 template<class T>
-bool TempList<T>::isFull() const {
+bool TimerList<T>::isFull() const {
    return t_size >= t_capacity;
 }
 
 template<class T>
-Timer& TempList<T>::operator [](int index) {
+Timer& TimerList<T>::operator [](int index) {
    return at(index);
 }
-
-
